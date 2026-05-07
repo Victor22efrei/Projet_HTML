aaa7784095efb8b1a6b974e848a040413db0f809
Teddy123451 <teddy.rame@efrei.net>
2026-05-07 20:27:58 +0200

    acceuil

--- js/accueil.js
+++ js/accueil.js
@@ -4,7 +4,7 @@ console.log("console");
 
 let images = [
     "../img/efrei.jpeg",
-    ".../img/diplome.jpeg",
+    "../img/diplome.jpeg",
     "../img/image1.png"
 ];
 
@@ -15,6 +15,14 @@ function changementImage() {
     i = (i + 1) % images.length;
 
     document.getElementById("photo").src = images[i];
+
+    let points = document.querySelectorAll(".point");
+
+    points.forEach(function(point) {
+        point.classList.remove("active");
+    });
+
+    points[i].classList.add("active");
 }
 
 
